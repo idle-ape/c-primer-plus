4c846014e7402a5271ca984a7450688b4d394c59
liuweifu <liuweifu@digifinex.com>
2025-04-16 07:23:40 +0000

    fnish chapter 1

---
+++ chapter01/demo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int dogs;
+    printf("How many dogs do you have?\n");
+    scanf("%d", &dogs);
+    printf("I have %d dogs.\n", dogs);
+    return 0;
+}
---
+++ chapter01/unit_convert.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define UNIT 2.54
+
+int main()
+{
+    int inch;
+    printf("This is a too to convert inch to centimeter.\nPlease input a number you want to convert: \n");
+    scanf("%d", &inch);
+    printf("%d inchs is equal to %.2f centimeters\n", inch, inch * UNIT);
+    return 0;
+}
