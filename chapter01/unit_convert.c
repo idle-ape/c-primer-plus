e61ffbddd48df1ef51a7e0dec466b64c782854f2
liuweifu <liuweifu@digifinex.com>
2025-04-16 07:28:26 +0000

    fix typo err

--- chapter01/unit_convert.c
+++ chapter01/unit_convert.c
@@ -5,7 +5,7 @@
 int main()
 {
     int inch;
-    printf("This is a too to convert inch to centimeter.\nPlease input a number you want to convert: \n");
+    printf("This is a tool that convert inch to centimeter.\nPlease input a number you want to convert: \n");
     scanf("%d", &inch);
     printf("%d inchs is equal to %.2f centimeters\n", inch, inch * UNIT);
     return 0;
